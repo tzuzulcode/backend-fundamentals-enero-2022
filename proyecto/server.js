70e2dce14af1ca4a50ed7360a00673b986496625
Tzuzul Code <mail@tzuzulcode.com>
2022-01-21 09:57:44 -0600

    Peticiones de tipo texto

--- proyecto/server.js
+++ proyecto/server.js
@@ -2,6 +2,16 @@ const express = require('express')
 
 const app = express()
 
+// Procesos intermedios
+// Middleware
+app.use(express.text()) // Cada vez que se haga uso de la app, se ejecute express.text()
+app.use(express.json()) // Cada vez que se haga uso de la app, se ejecute express.json()
+
+
+app.get('/',function(peticion,respuesta){
+    return respuesta.send("Pagina principal")
+})
+
 app.get('/saludo',function(peticion,respuesta){
     return respuesta.send("Hola mundo")
 })
@@ -9,7 +19,14 @@ app.get('/adios',function(peticion,respuesta){
     return respuesta.send("Adios mundo")
 })
 app.post('/saludo',function(peticion,respuesta){
-    return respuesta.send("Petición post. Hola")
+    const nombre = peticion.body
+    const mensaje = "Petición post. Hola, "+nombre
+    return respuesta.send(mensaje)
+})
+app.post('/saludo_json',function(peticion,respuesta){
+    const persona = peticion.body
+    const mensaje = "Petición post. Hola, "+persona.nombre
+    return respuesta.send(mensaje)
 })
 
 app.listen(4000,function(){
