91db4701172c4b1dd6c459ed1722dd3ad24ea982
Tzuzul Code <mail@tzuzulcode.com>
2022-02-25 10:09:35 -0600

    Handlebars

--- proyecto2/server.js
+++ proyecto2/server.js
@@ -2,16 +2,50 @@ const express = require("express")
 const { port } = require("./config")
 const {query} = require("./config/database")
 const {engine} = require("express-handlebars")
+const { DateTime } = require("luxon");
+
+// engine.registerHelper("formatDate",(date)=>{
+//     return DateTime.fromISO(date)
+// })
 
 const app = express()
 
-app.engine('handlebars',engine())
-app.set("view engine",'handlebars')
+// const hbs = handlebars.create({
+//     defaultLayout:null,
+//     extname:"hbs",
+//     // layoutsDir:"templates"
+//     helpers:{
+//         formatDate:function(date){
+//             const newDate = new DateTime(date)
+//             return newDate.toFormat("yyyy-MM-dd")
+//         }
+//     }
+// })
+
+
+app.engine('hbs',engine({
+    defaultLayout:null,
+    extname:"hbs",
+    // layoutsDir:"templates"
+    helpers:{
+        formatDate:function(date){
+            const newDate = new DateTime(date)
+            return newDate.toFormat("yyyy-MM-dd")
+        }
+    }
+}))
+    
+app.set("view engine",'hbs')
 app.set("views","views")
 
 app.get("/",async (req,res)=>{
+    const del = await query("DELETE FROM users")
     const users = await query("SELECT * FROM users")
-    return res.json(users)
+    return res.render("home",{
+        username:"tzuzulcode",
+        users,
+        hasUsers:users.length > 0
+    })
 })
 
 app.listen(port,function(){
